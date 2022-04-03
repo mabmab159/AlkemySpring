91745021ecf35c1a8d5c662a923f8a1e79b4099a
MabMab^^ <miguelberrioh@hotmail.com>
2022-04-03 01:49:48 -0500

    Cambios en bd

--- src/main/java/com/Spring/controller/UserController.java
+++ src/main/java/com/Spring/controller/UserController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class UserController {
 
     @Autowired
